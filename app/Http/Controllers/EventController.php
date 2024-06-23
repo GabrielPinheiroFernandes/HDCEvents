5d1589d02d9d02d93f751126a88612cdbb153116
MiahPika <162518373+MiahPika@users.noreply.github.com>
2024-06-23 19:34:23 -0300

    upando aula 17

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -15,7 +15,7 @@ class EventController extends Controller
     public function createEvent (){
         return view('events.create');
     }
-    
+
     public function store(request $request){
         $event = new  Event;
 
@@ -24,8 +24,25 @@ class EventController extends Controller
         $event->private = $request-> private;
         $event->description = $request-> description;
 
+        // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+
+        }
+
+
+
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg','Evento Criado Com Sucesso!');
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
         <div id="cards-container" class="row">
             @foreach ($events as $event)
                 <div class="card col-md-3">
-                    <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}" srcset="">
+                    <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}" srcset="">
                     <div class="card-body">
                         <p class="card-date">10/02/2003</p>
                         <h5 class="card-title">{{ $event->title }}</h5>
