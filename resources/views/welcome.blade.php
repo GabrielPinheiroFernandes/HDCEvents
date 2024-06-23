abed5dd58eecbf91a36898376fb2f636ed8bb293
MiahPika <162518373+MiahPika@users.noreply.github.com>
2024-06-23 16:24:18 -0300

    HDCEvents

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.main') 
+
+@section('title','HDC Events')
+
+@section('content')
+    <div class="col-md-12" id="search-container">
+        <h1>Busque um Evento</h1>
+        <form action="">
+            <input type="text" id="search" class="form-control" placeholder="procurar">
+        </form>
+    </div>    
+    <div id="events-container" class="col-md-12">
+        <h2>proximos eventos</h2>
+        <p class="subtitle">veja os eventos dos próximos dias</p>
+        <div id="cards-container" class="row">
+            @foreach ($events as $event)
+                <div class="card col-md-3">
+                    <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}" srcset="">
+                    <div class="card-body">
+                        <p class="card-date">10/02/2003</p>
+                        <h5 class="card-title">{{ $event->title }}</h5>
+                        <p class="card-participants">x Participantes</p>
+                        <a href="#" class="btn btn-primary">Saber mais</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
+ 
